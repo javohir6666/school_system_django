6840c61fb923870dfbba9038a223e923d69bb6e9
Javohir <javohir6666@gmail.com>
2022-11-04 22:46:55 +0500

    first

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,3 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
@@ -21,15 +6,31 @@ from school_system_app import views, hodView
 urlpatterns = [
     path('demo/', views.showDemoPage, name='demo'),
     path('admin_home', hodView.hod_View, name='hod_view'),
-    path('add_staff', hodView.add_staff),
+
     path('', views.ShowLoginPage, name="home"),
     path('get_user_details', views.get_user_details),
     path('logout_user', views.logout_user),
     path('dologin', views.doLogin, name="doLogin"),
     path('admin/', admin.site.urls),
-    path('add_staff_save', hodView.add_staff_save),
+    # COURSE
     path('add_course', hodView.add_course),
     path('add_course_save', hodView.add_course_save),
+    path('manage_courses', hodView.manage_courses),
+    # SUBJECT
+    path('add_subject', hodView.add_subject),
+    path('manage_subjects', hodView.manage_subjects),
+    path('add_subject_save', hodView.add_subject_save),
+    # STAFF
+    path('add_staff', hodView.add_staff),
+    path('add_staff_save', hodView.add_staff_save),
+    path('edit_staff/<str:staff_id>/', hodView.edit_staff),
+    path('manage_staffs', hodView.manage_staffs),
+    #STUDENT
     path('add_student', hodView.add_student),
+    path('manage_students', hodView.manage_students),
+    path('edit_student/<str:student_id>/', hodView.edit_student),
     path('add_student_save', hodView.add_student_save),
+    path('edit_student_save', hodView.edit_student_save),
+    
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- school_system_app/hodView.py
+++ school_system_app/hodView.py
@@ -1,23 +1,120 @@
-from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
-from school_system_app.models import Courses, CustomUser
-
+from school_system_app.models import *
+from django.core.files.storage import FileSystemStorage
 
 def hod_View(request):
     return render(request, "hod_templates/home_content.html")
 
+
+# STUDENT
 def add_student(request):
     courses = Courses.objects.all
     response ={'courses': courses}
     return render(request, "hod_templates/add_student.html", response)
 
+
+def add_student_save(request):
+    if request.method != "POST":
+        return HttpResponse("Method not allowed")
+    else:
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        username = request.POST.get("username")
+        address = request.POST.get("address")
+        gender = request.POST.get("gender")
+        session_start = request.POST.get("session_start")
+        session_end = request.POST.get("session_end")
+        course_id = request.POST.get("course")
+        if request.FILES['image']:
+            file = request.FILES['image']
+            fs = FileSystemStorage()
+            filename = fs.save(file.name, file)
+            profile_pic_url = fs.url(filename)
+
+        try:
+            user = CustomUser.objects.create_user(
+                username=username,
+                first_name=first_name,
+                last_name=last_name,
+                password=password,
+                email=email,
+                user_type=3 
+            )
+            user.students.gender = gender
+            user.students.session_start_year=session_start
+            user.students.session_end_year=session_end 
+            user.students.address = address
+            user.profile_pic = profile_pic_url
+            user.save()
+            messages.success(request, "Successfully added Student")
+            return HttpResponseRedirect("/add_student")
+        except:
+            messages.error(request, "Failed to add Student")
+            return HttpResponseRedirect("/add_student")
+
+
+def manage_students(request):
+    students = Students.objects.all()
+    return render(request, "hod_templates/manage_students.html",{"students": students})
+
+
+def edit_student(request, student_id):
+    courses = Courses.objects.all()
+    student = Students.objects.get(id=student_id)
+    return render(request, "hod_templates/edit_student.html", {"student": student,"courses": courses})
+
+def edit_student_save(request):
+    if request.method != "POST":
+        return HttpResponse("Method not allowed")
+    else:
+        student_id  = request.POST.get("student_id")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        address = request.POST.get("address")
+        gender = request.POST.get("gender")
+        session_start = request.POST.get("session_start")
+        session_end = request.POST.get("session_end")
+        course_id = request.POST.get("course")
+        
+        if request.FILES['image-upload']:
+            file = request.FILES['image-upload']
+            fs = FileSystemStorage()
+            filename = fs.save(file.name, file)
+            profile_pic_url = fs.url(filename)
+        else:
+            profile_pic_url = None
+        try:
+            user = CustomUser.objects.get(id=student_id)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.username = username
+            user.save()
+            student = Students.objects.get(admin_id=student_id)
+            student.address = address
+            student.gender = gender
+            student.profile_pic = profile_pic_url
+            student.session_start_year = session_start
+            student.session_end_year = session_end
+            course = Courses.objects.get(id=course_id)
+            student.course_id = course
+            student.save()
+            messages.success(request, "Successfully edited Student")
+            return HttpResponseRedirect("/edit_student/" + student_id)
+        except:
+            messages.error(request, "Failed to edit Student")
+            return HttpResponseRedirect("/edit_student/" + student_id)
+
+
+# STAFF
 def add_staff(request):
     return render(request, "hod_templates/add_staff.html")
-
-def add_course(request):
-    return render(request, "hod_templates/add_course.html")
 
 
 def add_staff_save(request):
@@ -47,6 +144,22 @@ def add_staff_save(request):
             messages.error(request, "Failed to add Staff")
             return HttpResponseRedirect("/add_staff")
 
+
+def manage_staffs(request):
+    staffs = Staffs.objects.all()
+    return render(request, "hod_templates/manage_staffs.html",{"staffs": staffs})
+
+def edit_staff(request, staff_id):
+    staffs = Staffs.objects.get(admin=staff_id)
+    return render(request, "hod_templates/edit_staff.html",{"staffs":staffs})
+
+
+
+# COURSE
+def add_course(request):
+    return render(request, "hod_templates/add_course.html")
+
+
 def add_course_save(request):
     if request.method != "POST":
         return HttpResponse("Method not allowed")
@@ -61,37 +174,28 @@ def add_course_save(request):
             messages.error(request, "Failed to add Course")
             return HttpResponseRedirect("/add_course")
 
-def add_student_save(request):
-    if request.method != "POST":
-        return HttpResponse("Method not allowed")
-    else:
-        first_name = request.POST.get("first_name")
-        last_name = request.POST.get("last_name")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        username = request.POST.get("username")
-        address = request.POST.get("address")
-        gender = request.POST.get("gender")
-        session_start = request.POST.get("session_start")
-        session_end = request.POST.get("session_end")
-        course_id = request.POST.get("course")
-        try:
-            user = CustomUser.objects.create_user(
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                password=password,
-                email=email,
-                user_type=3 
-            )
-            user.students.gender = gender
-            user.students.session_start_year=session_start
-            user.students.session_end_year=session_end 
-            user.students.profile_pic=""
-            user.students.address = address
-            user.save()
-            messages.success(request, "Successfully added Student")
-            return HttpResponseRedirect("/add_student")
-        except:
-            messages.error(request, "Failed to add Student")
-            return HttpResponseRedirect("/add_student")
+
+def edit_course(request):
+    return render(request, "hod_templates/edit_course.html")
+
+def manage_courses(request):
+    return render(request, "hod_templates/manage_courses.html")
+
+
+# SUBJECT
+
+def add_subject(request):
+    return render(request, "hod_templates/add_subject.html")
+
+
+def add_subject_save(request):
+    pass
+
+
+def manage_subjects(request):
+    pass
+
+
+def edt_subject(request):
+    pass
+
